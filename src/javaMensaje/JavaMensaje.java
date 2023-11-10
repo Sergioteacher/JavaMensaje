8f6e40d3a39c7e93c745ed5fa6932cb7a7529867
Sergio Teacher <Sergio Teacher@MiniWin>
2023-11-10 16:54:35 +0100

    Inicio del proyecto

---
+++ src/javaMensaje/JavaMensaje.java
@@ -0,0 +1,43 @@
+package javaMensaje;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class JavaMensaje {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					JavaMensaje window = new JavaMensaje();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public JavaMensaje() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
